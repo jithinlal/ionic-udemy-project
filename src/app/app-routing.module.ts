54b9390d216b8b4da5aba4d6656b74f6a36bda5c
jithinlal <jithtitan007@gmail.com>
2019-04-10 00:04:30 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+	{ path: '', redirectTo: 'places', pathMatch: 'full' },
+	{ path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+	{ path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+	{
+		path: 'bookings',
+		loadChildren: './bookings/bookings.module#BookingsPageModule'
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
