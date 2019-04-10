671bd6440ceba29a72218668bdd2bb5cc99b78bd
jithinlal <jithtitan007@gmail.com>
2019-04-10 20:51:20 +0530

    second commit

--- src/app/app/places/places.page.ts
+++ src/app/app/places/places.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-places',
 	templateUrl: './places.page.html',
-	styleUrls: ['./places.page.scss']
+	styleUrls: ['./places.page.scss'],
 })
 export class PlacesPage implements OnInit {
 	constructor() {}
---
+++ src/app/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class PlacesService {
+	private _places: Place[] = [
+		new Place(
+			'p1',
+			'Manhattan',
+			'In the heart of New York city.',
+			'https://cdn.traveltripper.io/site-assets/192_680_16182/media/2018-05-27-000645/mts-neighborhood-guide-midtown-manhattan.png',
+			149.99
+		),
+		new Place(
+			'p2',
+			'Kochi',
+			'South India.',
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Kochi-725765.jpg/350px-Kochi-725765.jpg',
+			49.99
+		),
+		new Place(
+			'p3',
+			'Paris',
+			'The best place in the world.',
+			'https://www.telegraph.co.uk/content/dam/Travel/hotels/europe/france/paris/paris-cityscape-overview-guide.jpg?imwidth=1400',
+			399.89
+		)
+	];
+
+	get places() {
+		return [...this._places];
+	}
+
+	constructor() {}
+}
